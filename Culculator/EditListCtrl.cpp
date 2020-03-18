937230f5158be0a1b9862c265b68fe0b83f8e3bf
LeiWeibin207 <32983736+LeiWeibin207@users.noreply.github.com>
2020-03-18 23:52:12 +0800

    Add files via upload

---
+++ Culculator/EditListCtrl.cpp
@@ -0,0 +1,29 @@
+// EditListCtrl.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "EditListCtrl.h"
+
+
+// CEditListCtrl
+
+IMPLEMENT_DYNAMIC(CEditListCtrl, CListCtrl)
+
+CEditListCtrl::CEditListCtrl()
+{
+
+}
+
+CEditListCtrl::~CEditListCtrl()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CEditListCtrl, CListCtrl)
+END_MESSAGE_MAP()
+
+
+
+// CEditListCtrl 消息处理程序
+
+
---
+++ Culculator/EditListCtrl.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// CEditListCtrl
+
+class CEditListCtrl : public CListCtrl
+{
+	DECLARE_DYNAMIC(CEditListCtrl)
+
+public:
+	CEditListCtrl();
+	virtual ~CEditListCtrl();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
